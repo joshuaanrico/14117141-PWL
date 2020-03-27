be62a8af625ee314c2eb9b88bcc6525a0028baa0
Laurensius Joshua Anrico Agustinus <joshua.anrico30@gmail.com>
2020-03-27 07:15:57 +0700

    prak minggu 4 blom slesai

---
+++ Latihan_Mg4/hw.js
@@ -0,0 +1,13 @@
+console.log("Hello World");
+
+let	nama_panjang = [{nama:"joshua"},{nama:"anrico"}];
+console.log(nama_panjang);
+
+console.table([[1,2,3],[4,5,6],[7,8,9]]);
+
+//umur blom
+for(let umur of umurs){
+	if (umur_gw.umur < 23) {
+		console.log()
+	}
+}
